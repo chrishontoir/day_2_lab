a4cc2553171430423213dd9230363475b71400a5
user <user@users-MacBook-Pro.local>
2018-11-27 13:57:47 +0000

    month numbers

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -36,3 +36,34 @@ def add_string_as_number(string1, string2)
   total = num1 + num2
   return total
 end
+
+def number_to_full_month_name(num)
+  case num
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  else
+    return "Invalid month"
+  end
+end
