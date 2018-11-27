97d2ea8da47f981bf3d36e8952b0dc79e6808b8c
user <user@users-MacBook-Pro.local>
2018-11-27 15:04:17 +0000

    Corrected

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -70,36 +70,41 @@ def number_to_full_month_name(num)
   end
 end
 
-def number_to_short_month_name(num)
-  case num
-  when 1
-    return "Jan"
-  when 2
-    return "Feb"
-  when 3
-    return "Mar"
-  when 4
-    return "Apr"
-  when 5
-    return "May"
-  when 6
-    return "Jun"
-  when 7
-    return "Jul"
-  when 8
-    return "Aug"
-  when 9
-    return "Sep"
-  when 10
-    return "Oct"
-  when 11
-    return "Nov"
-  when 12
-    return "Dec"
-  else
-    return "Invalid month"
-  end
+def number_to_short_month_name(num2)
+  month = number_to_full_month_name(num2).slice(0,3)
+  return month
 end
+
+# def number_to_short_month_name(num)
+#   case num
+#   when 1
+#     return "Jan"
+#   when 2
+#     return "Feb"
+#   when 3
+#     return "Mar"
+#   when 4
+#     return "Apr"
+#   when 5
+#     return "May"
+#   when 6
+#     return "Jun"
+#   when 7
+#     return "Jul"
+#   when 8
+#     return "Aug"
+#   when 9
+#     return "Sep"
+#   when 10
+#     return "Oct"
+#   when 11
+#     return "Nov"
+#   when 12
+#     return "Dec"
+#   else
+#     return "Invalid month"
+#   end
+# end
 
 def calc_volume_of_cube(side_length)
   return side_length**3
@@ -112,8 +117,3 @@ end
 def fahrenheit_to_celsius(temp)
   return ((temp-32)*(5.0/9.0))
 end
-
-puts calc_volume_of_sphere(10)
-
-
-puts Math::PI
